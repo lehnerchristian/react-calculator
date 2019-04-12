c9b482c7304cf7093a724a131d11d21ee5599326
Christian Lehner <christian.lehner@datavisyn.io>
2019-04-12 12:55:09 +0200

    added snapshot testing to Keypad component

--- src/components/Keypad/Keypad.test.tsx
+++ src/components/Keypad/Keypad.test.tsx
@@ -8,6 +8,8 @@ describe('Keypad', () => {
   // create mock functions with jest.fn()
   beforeEach(() => (wrapper = shallow(<Keypad callOperator={jest.fn()} numbers={[]} operators={[]} setOperator={jest.fn()} updateDisplay={jest.fn()} />)));
 
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
