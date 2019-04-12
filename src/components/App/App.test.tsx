991d7c140f649694f3ab69fd66fa7fed1c6c6df3
Christian Lehner <christian.lehner@datavisyn.io>
2019-04-12 11:14:05 +0200

    added snapshot testing

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -7,6 +7,9 @@ describe('App', () => {
   let wrapper: ShallowWrapper;
   beforeEach(() => (wrapper = shallow(<App />)));
 
+  // add snapshot testing - this stores a snapshot of the current virtual DOM of this component
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
